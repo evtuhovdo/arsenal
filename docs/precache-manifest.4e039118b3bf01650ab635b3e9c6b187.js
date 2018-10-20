a58e04b9e803adff76fb91dae3e594d7195b48f8
Denis Evtukhov <evtuhovdo@gmail..com>
2018-10-20 17:29:11 +0700

    Добавлена папка docs

---
+++ docs/precache-manifest.4e039118b3bf01650ab635b3e9c6b187.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba401ca1843004835017fa3025ee6f1c",
+    "url": "/static/media/popup_nipple.ba401ca1.svg"
+  },
+  {
+    "revision": "c56018f80dc364c8ce0f",
+    "url": "/static/css/main.d44dd8c7.chunk.css"
+  },
+  {
+    "revision": "e365e00708ffb90fbb23f477e1fc7831",
+    "url": "/static/media/girl.e365e007.png"
+  },
+  {
+    "revision": "bb6dce28018e2054404b",
+    "url": "/static/js/1.bb6dce28.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de7462bb6191168f2d87e287fbf1bf60",
+    "url": "/static/media/1.de7462bb.png"
+  },
+  {
+    "revision": "9bdec9ca87524d19e6ad60e38356f91a",
+    "url": "/static/media/3.9bdec9ca.png"
+  },
+  {
+    "revision": "4fe44f63dfe8a4281e8db7eba77788c2",
+    "url": "/static/media/0.4fe44f63.png"
+  },
+  {
+    "revision": "1516ca3d0e61c9d62f45deb4a5831a01",
+    "url": "/static/media/2.1516ca3d.png"
+  },
+  {
+    "revision": "fb850f4ab0f828207b977949dc50bbf3",
+    "url": "/static/media/4.fb850f4a.png"
+  },
+  {
+    "revision": "47c32ceee75dbc2aded85131f4d59eba",
+    "url": "/static/media/1.47c32cee.png"
+  },
+  {
+    "revision": "71a03685b346b9ba106fbfe7113448f8",
+    "url": "/static/media/2.71a03685.png"
+  },
+  {
+    "revision": "c56018f80dc364c8ce0f",
+    "url": "/static/js/main.c56018f8.chunk.js"
+  },
+  {
+    "revision": "19921dd5b19bc5cf0f7bfa90e01e7d03",
+    "url": "/static/media/box.19921dd5.png"
+  },
+  {
+    "revision": "2c8791010560aedb9af303b80d06b2e1",
+    "url": "/static/media/item-1.2c879101.png"
+  },
+  {
+    "revision": "f4d9b5b688a959b5e4d008345a2f2f39",
+    "url": "/static/media/item-5.f4d9b5b6.png"
+  },
+  {
+    "revision": "1c88f739e8071a7401811ecad5ab0616",
+    "url": "/static/media/item-4.1c88f739.png"
+  },
+  {
+    "revision": "c26050a63f62c579dfce37aba440bfb8",
+    "url": "/static/media/item-2.c26050a6.png"
+  },
+  {
+    "revision": "66899ff09235f3e3f4f6d3324b57ee4c",
+    "url": "/static/media/item-3.66899ff0.png"
+  },
+  {
+    "revision": "4d1a94047039add4f9ea1050cf4a66a1",
+    "url": "/static/media/item-6.4d1a9404.png"
+  },
+  {
+    "revision": "335aba9d32d4dd27058d2ed6a6ed2620",
+    "url": "/static/media/vac_tupe.335aba9d.png"
+  },
+  {
+    "revision": "ce9653669074413143f698eae2ca6408",
+    "url": "/static/media/bg.ce965366.png"
+  },
+  {
+    "revision": "bb6dce28018e2054404b",
+    "url": "/static/css/1.3b9cf9e4.chunk.css"
+  },
+  {
+    "revision": "24f03cf14aeb0f3c8f3fa26303255f1d",
+    "url": "/index.html"
+  }
+];
